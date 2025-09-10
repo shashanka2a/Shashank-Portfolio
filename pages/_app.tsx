b50af6f916927a63148ac6558c2bd0e8bd6290c6
shashanka2a <37143174+shashanka2a@users.noreply.github.com>
2025-09-10 17:42:07 -0400

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { Analytics } from '@vercel/analytics/react'
 import '../src/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }
